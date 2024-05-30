f9b684ff3a13130c8b177213a5de051456657308
Owen <67910741+owen300@users.noreply.github.com>
2024-05-30 08:27:03 -0400

    depreciated state and wrappedcommand, dont plan on using them
    Signed-off-by: Owen <67910741+owen300@users.noreply.github.com>

--- src/main/java/frc/utils/WrappedCommand.java
+++ src/main/java/frc/utils/WrappedCommand.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.State;
 import frc.robot.State.robotState;
 
-public class WrappedCommand extends Command{
+public class WrappedCommand extends Command{//depreciated, dont plan on using
     @Override
     public void end(boolean interrupted){
         State.setState(robotState.Stable);
