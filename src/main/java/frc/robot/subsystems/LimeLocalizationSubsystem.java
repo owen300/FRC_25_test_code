6543630d1504de875a04de4940edb0aeea8c73ef
Owen <owenfriedman07@gmail.com>
2024-08-27 18:54:55 -0400

    massive vision changes, sdtdev now scales to the distance and is more than slighly over complicated

--- src/main/java/frc/robot/subsystems/LimeLocalizationSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLocalizationSubsystem.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.utils.LimelightHelpers;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.numbers.N3;
 
 public class LimeLocalizationSubsystem{
     public String name= "";
@@ -20,9 +23,29 @@ public class LimeLocalizationSubsystem{
     }
     public double time=0;
 
+    public  Vector<N3> getstdev() {
+      LimelightHelpers.PoseEstimate mt2=LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(name);
+    if (mt2.tagCount>1) {
+      return VecBuilder.fill(0.65,0.65,0.999999);
+    }else if(mt2.avgTagDist>5){
+        return VecBuilder.fill(0.9,0.9,0.99999);
+      }else{
+        if(mt2.avgTagDist>4){
+          return VecBuilder.fill(0.85,0.85,0.999999);
+        }else if(mt2.avgTagDist>3){
+            return VecBuilder.fill(0.75,0.75,0.999999);
+        }else if(mt2.avgTagDist>2){
+          return VecBuilder.fill(0.65,0.65,0.999999);
+        }else{
+          return VecBuilder.fill(0.6,0.6,0.999999);
+        }
+      }
+    }
+
+    
     public Optional<Pose2d> getPose(){
         boolean doRejectUpdate=false;
-    LimelightHelpers.SetRobotOrientation("name", sd.SwerveDriveSubsystem.swerveDrivePoseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+    LimelightHelpers.SetRobotOrientation(name, sd.SwerveDriveSubsystem.swerveDrivePoseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
     LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("lime1");
     if(Math.abs(sd.pigeon.getAngularVelocityYWorld().getValue()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
     {
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -3,13 +3,21 @@ package frc.robot.subsystems;
 import java.util.Optional;
 
 import org.photonvision.EstimatedRobotPose;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.utils.LimelightHelpers;
 
 public class VisionSubsystem extends SubsystemBase{
+    AprilTagFieldLayout aprilTagFieldLayout;
+    private final double visionRatio = 10;
     public final LimeLocalizationSubsystem limeR= new LimeLocalizationSubsystem("limeR");
     public final LimeLocalizationSubsystem limeL= new LimeLocalizationSubsystem("limeL");
     public final PhotonSubsystem photonR=new PhotonSubsystem();
@@ -38,26 +46,60 @@ public class VisionSubsystem extends SubsystemBase{
     }
 
     public void updateFromLimeL(){
-        if(!limeLpose().isEmpty())sd.SwerveDriveSubsystem.addVisionMeasurement(limeLpose().get(), limeL.time,VecBuilder.fill(.7,.7,9999999));
+        if(!limeLpose().isEmpty())sd.SwerveDriveSubsystem.addVisionMeasurement(limeLpose().get(), limeL.time,limeL.getstdev());
     }
 
     public void updateFromLimeR(){
-        if(!limeRpose().isEmpty())sd.SwerveDriveSubsystem.addVisionMeasurement(limeRpose().get(), limeR.time,VecBuilder.fill(.7,.7,9999999));
+        if(!limeRpose().isEmpty())sd.SwerveDriveSubsystem.addVisionMeasurement(limeRpose().get(), limeR.time,limeR.getstdev());
     }
 
     public void updatePhotonL(){
         Optional<EstimatedRobotPose> data=photonL.photonPose.update();
+        double sum=0;
+        for (PhotonTrackedTarget target :data.get().targetsUsed) {
+            Translation2d tagPosition = aprilTagFieldLayout.getTagPose(target.getFiducialId()).get()
+                    .getTranslation().toTranslation2d();
+            sum += data.get().estimatedPose.toPose2d().getTranslation().getDistance(tagPosition);
+        }
+        sum/=data.get().targetsUsed.size();
     if(!data.isEmpty()){//if there is data from photon vision this runs and updates the odometry with its pose
       SmartDashboard.putString("PhotonL pose",data.get().estimatedPose.toPose2d().toString());//for testing
-      sd.SwerveDriveSubsystem.addVisionMeasurement(data.get().estimatedPose.toPose2d(),data.get().timestampSeconds);//adds in vision
+      sd.SwerveDriveSubsystem.addVisionMeasurement(data.get().estimatedPose.toPose2d(),data.get().timestampSeconds,getstdev(data.get().targetsUsed.size(), sum));//adds in vision
     }
     }
 
     public void updatePhotonR(){
         Optional<EstimatedRobotPose> data=photonR.photonPose.update();
+        double sum=0;
+        for (PhotonTrackedTarget target :data.get().targetsUsed) {
+            Translation2d tagPosition = aprilTagFieldLayout.getTagPose(target.getFiducialId()).get()
+                    .getTranslation().toTranslation2d();
+            sum += data.get().estimatedPose.toPose2d().getTranslation().getDistance(tagPosition);
+        }
+        sum/=data.get().targetsUsed.size();
     if(!data.isEmpty()){//if there is data from photon vision this runs and updates the odometry with its pose
-      SmartDashboard.putString("PhotonR pose",data.get().estimatedPose.toPose2d().toString());//for testing
-      sd.SwerveDriveSubsystem.addVisionMeasurement(data.get().estimatedPose.toPose2d(),data.get().timestampSeconds);//adds in vision
+      SmartDashboard.putString("PhotonRmm pose",data.get().estimatedPose.toPose2d().toString());//for testing
+      sd.SwerveDriveSubsystem.addVisionMeasurement(data.get().estimatedPose.toPose2d(),data.get().timestampSeconds,getstdev(data.get().targetsUsed.size(), sum));//adds in vision
     }
     }
+
+    public  Vector<N3> getstdev(double tags, double avgDistance) {
+      if (tags>1) {
+        return VecBuilder.fill(0.65,0.65,0.999999);
+      }else if(avgDistance>5){
+          return VecBuilder.fill(0.9,0.9,0.99999);
+        }else{
+          if(avgDistance>4){
+            return VecBuilder.fill(0.85,0.85,0.999999);
+          }else if(avgDistance>3){
+              return VecBuilder.fill(0.75,0.75,0.999999);
+          }else if(avgDistance>2){
+            return VecBuilder.fill(0.65,0.65,0.999999);
+          }else{
+            return VecBuilder.fill(0.6,0.6,0.999999);
+          }
+        }
+      }
+
+
 }
